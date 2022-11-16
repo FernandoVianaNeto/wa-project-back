9a522a6254750f572ba9eef030bddf4d10f9b550
Fernando Viana <fernando.neto-loldesign@fretebras.com.br>
2022-11-16 20:33:28 -0300

    refactor: movies module

--- src/modules/movies/movies.module.ts
+++ src/modules/movies/movies.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MoviesController } from './controllers/movies.controller';
 import { Movie, MovieSchema } from './entities/movies.entity';
+import { MoviesService } from './movies.service';
+import { MovieRepository } from './repositories/movie.repository';
 
 @Module({
   imports: [
